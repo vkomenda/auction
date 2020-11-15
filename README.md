347dce6b2efe6fea88dbb1ebe622a404ecbd5b36
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-11-15 00:39:22 +0000

    basic state management and instruction handling

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
-## Solana Auction
+## Solana Auction **WIP**
 
 
 ### Overview
 
-The Auction is a simple Solana program allowing a SOL holder address to start an auction on a Solana
-network. Once it is started, an arbitrary number of SOL holder addresses can submit their bids until
-the deadline that is set in the auction.
+The Auction is a simple Solana program allowing an address to start an auction on a Solana
+network. Once it is started, an arbitrary number addresses can submit their bids until the deadline
+that is set in the auction.
+
+This project is based on the Solana [example-helloworld](https://github.com/solana-labs/example-helloworld).
 
 
 #### Supported Auction Type
 
-Currently only a type of second-price auction is supported which is a simplified version of the
-[eBay auction](https://en.wikipedia.org/wiki/EBay#Auction-style_listings). The rules of this auction
+Currently only a type of English auction is supported. The rules of this auction
 are:
 
-* The winning bidder pays the second-highest bid.
-* All bids but the second one are sealed. The second bid is readable throughout the auction to allow
-  price discovery.
-* The auction has a start time and an end time, the deadline after which bids are no longer accepted
-  and the winner is announced.
+* The winner is the bidder with the highest bid.
+* All bids but the highest one are sealed. The highest bid is readable throughout the auction to
+  allow price discovery.
+* The auction has a start time (the time of deployment) and an end time, the deadline after which
+  bids are no longer accepted and the winner is announced.
 
-As it is common in sealed bid auctions, it is the bidders' advantage to make bids only at the very
+As it is common in ascending auctions, it is the bidders' advantage to make bids only at the very
 end of the auction since early bids only increase the final price.
 
 
@@ -31,6 +32,7 @@ end of the auction since early bids only increase the final price.
 * [Rust](https://www.rust-lang.org/tools/install)
 * `curl` or `wget` is used by the BPF SDK install script.
 
+
 ### Quick Start
 
 ```bash
@@ -39,7 +41,10 @@ npm run program:build
 npm run client:start
 ```
 
+### WIP Status
 
-### Test
+The Solana program is very basic and is currently in development.
 
-TODO
+The client is missing basic required features.
+
+Tests are missing.
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,90 +1,150 @@
 #![cfg(feature = "program")]
 
 use byteorder::{ByteOrder, LittleEndian};
+use num_derive::FromPrimitive;
 use solana_sdk::{
     account_info::{next_account_info, AccountInfo},
+    decode_error::DecodeError,
     entrypoint,
     entrypoint::ProgramResult,
     info,
     program_error::ProgramError,
+    program_pack::{Pack, Sealed},
     pubkey::Pubkey,
+    timing::timestamp,
 };
+use std::convert::TryInto;
 use std::mem;
+use thiserror::Error;
+
+type Bid = u32;
+
+/// The auction summary length.
+const SUMMARY_DATA_LEN: usize = 8 + 32 + 4;
+
+#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+enum Error {
+    #[error("Auction Ended")]
+    AuctionEnded,
+}
+
+impl From<Error> for ProgramError {
+    fn from(e: Error) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
+
+impl<T> DecodeError<T> for Error {
+    fn type_of() -> &'static str {
+        "Error"
+    }
+}
+
+/// Public auction information.
+// TODO: split into read-only and writable parts for better performance.
+struct AuctionSummary {
+    /// The deadline timestamp.
+    deadline: u64,
+    /// Winning bid value.
+    winning_bid: Bid,
+    /// Winning bidder Pubkey.
+    winning_pubkey: Pubkey,
+}
+
+impl Sealed for AuctionSummary {}
+
+impl Pack for AuctionSummary {
+    const LEN: usize = SUMMARY_DATA_LEN;
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        Ok(AuctionSummary {
+            deadline: LittleEndian::read_u64(&src[0..7]),
+            winning_bid: LittleEndian::read_u32(&src[8..11]),
+            winning_pubkey: Pubkey::new(&src[12..43]),
+        })
+    }
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        LittleEndian::write_u64(&mut dst[0..7], self.deadline);
+        LittleEndian::write_u32(&mut dst[8..11], self.winning_bid);
+        mem::replace::<[u8; 32]>(
+            &mut dst[12..43].try_into().expect("cannot copy pubkey"),
+            Pubkey::to_bytes(self.winning_pubkey),
+        );
+    }
+}
+
+/// Program instructions.
+// TODO: Add an instruction to cancel a bid. Perhaps remove GetSummary.
+enum Instruction {
+    /// Get the auction deadline and the current maximum bid pubkey and value.
+    GetSummary,
+    /// Place a bid.
+    PlaceBid(Bid),
+}
+
+impl Sealed for Instruction {}
+
+impl Pack for Instruction {
+    const LEN: usize = 1 + 4;
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        match src[0] {
+            0 => Ok(Instruction::GetSummary),
+            1 => Ok(Instruction::PlaceBid(LittleEndian::read_u32(&src[1..4]))),
+            _ => Err(ProgramError::InvalidInstructionData),
+        }
+    }
+
+    fn pack_into_slice(&self, _dst: &mut [u8]) {}
+}
 
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
-// Program entrypoint's implementation
+// Program entrypoint's implementation.
 fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+    program_id: &Pubkey,      // Public key of the auction account.
+    accounts: &[AccountInfo], // Accounts: 1) auction, 2) bidder.
+    instruction_data: &[u8],  // Instruction
 ) -> ProgramResult {
     info!("Auction BPF program entrypoint");
-
-    // Iterating accounts is safer then indexing
+    let t = timestamp();
+    let instruction = Instruction::unpack_unchecked(&instruction_data)?;
     let accounts_iter = &mut accounts.iter();
-
-    // Get the account to say hello to
-    let account = next_account_info(accounts_iter)?;
-
-    // The account must be owned by the program in order to modify its data
-    if account.owner != program_id {
+    let auction_account = next_account_info(accounts_iter)?;
+    if auction_account.owner != program_id {
         info!("Auction account does not have the correct program id");
         return Err(ProgramError::IncorrectProgramId);
     }
-
-    // The data must be large enough to hold a u64 count
-    if account.try_data_len()? < mem::size_of::<u32>() {
-        info!("Greeted account data length too small for u32");
-        return Err(ProgramError::InvalidAccountData);
+    if auction_account.try_data_len()? < SUMMARY_DATA_LEN {
+        info!("Auction account data length is too small");
+        return Err(ProgramError::AccountDataTooSmall);
     }
-
-    // Increment and store the number of times the account has been greeted
-    let mut data = account.try_borrow_mut_data()?;
-    let mut num_greets = LittleEndian::read_u32(&data);
-    num_greets += 1;
-    LittleEndian::write_u32(&mut data[0..], num_greets);
-
-    info!("Hello!");
-
-    Ok(())
+    let mut summary_data = auction_account.try_borrow_mut_data()?;
+    let mut summary = AuctionSummary::unpack_unchecked(&summary_data)?;
+    match instruction {
+        Instruction::GetSummary => {
+            // TODO
+        }
+        Instruction::PlaceBid(bid) => {
+            if t > summary.deadline {
+                return Err(Error::AuctionEnded.into());
+            }
+            let bidder_account = next_account_info(accounts_iter)?;
+            let bidder_pubkey = bidder_account.unsigned_key().clone();
+            if bid > summary.winning_bid {
+                summary.winning_bid = bid;
+                summary.winning_pubkey = bidder_pubkey;
+            }
+        }
+    }
+    AuctionSummary::pack(summary, &mut summary_data)
 }
 
-// Sanity tests
 #[cfg(test)]
 mod test {
-    use super::*;
-    use solana_sdk::clock::Epoch;
-
-    #[test]
-    fn test_sanity() {
-        let program_id = Pubkey::default();
-        let key = Pubkey::default();
-        let mut lamports = 0;
-        let mut data = vec![0; mem::size_of::<u64>()];
-        LittleEndian::write_u64(&mut data, 0);
-        let owner = Pubkey::default();
-        let account = AccountInfo::new(
-            &key,
-            false,
-            true,
-            &mut lamports,
-            &mut data,
-            &owner,
-            false,
-            Epoch::default(),
-        );
-        let instruction_data: Vec<u8> = Vec::new();
-
-        let accounts = vec![account];
-
-        assert_eq!(LittleEndian::read_u64(&accounts[0].data.borrow()), 0);
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(LittleEndian::read_u64(&accounts[0].data.borrow()), 1);
-        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(LittleEndian::read_u64(&accounts[0].data.borrow()), 2);
-    }
+    // TODO
 }
 
 // Required to support info! in tests
